0a7f12efd2d1ada2bafd95e7223cf6ba4f000e75
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-24 23:47:08 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const hbs = require("hbs");
+
+let app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to our website!"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "This is a bad page"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Starting server on port 3000");
+});
