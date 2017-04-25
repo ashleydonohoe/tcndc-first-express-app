79c15908e09c9fb8d8fd83023fdbe88fd227956b
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-25 00:03:38 -0400

    added custom middleware

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 let app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs", {
+//     pageTitle: "Page Not Available",
+//     errorMessage: "This page is under construction"
+//   })
+// });
+
 app.use(express.static(__dirname + "/public"));
+
 
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
