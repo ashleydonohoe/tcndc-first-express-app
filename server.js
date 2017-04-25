7535251c994d4fd51ff2147120c84e8279557452
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-25 00:17:43 -0400

    Added Projects Page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get("/bad", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects Page"
+  });
+});
+
 app.listen(port, () => {
   console.log(`Starting server on port ${port}`);
 });
